7670306acd44a2be73ca1c1f96b02283ee40528c
Arjanit <ak35372@universum-ks.org>
2020-12-31 23:20:56 +0100

    Added the Edit screen

---
+++ srecruiter_app/lib/screens/edit_student_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditStudentScreen extends StatefulWidget {
+  @override
+  _EditStudentScreenState createState() => _EditStudentScreenState();
+}
+
+class _EditStudentScreenState extends State<EditStudentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
